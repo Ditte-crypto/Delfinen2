8db3ba32311ee9f7e50b2c2c6ed802055c4d60ac
Ditte-crypto <60691158+Ditte-crypto@users.noreply.github.com>
2020-05-11 10:57:53 +0200

    nyt

--- src/main/java/Model/MedlemsBog.java
+++ src/main/java/Model/MedlemsBog.java
@@ -1,4 +1,11 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class MedlemsBog extends Bog {
+    ArrayList<Medlem> medlemmer;
+    public void tilføjMedlemTilBog(Medlem medlem) {
+        //tilføjer medlemsobject til medlemsliste
+        //lav medlem i databasen
+    }
 }
