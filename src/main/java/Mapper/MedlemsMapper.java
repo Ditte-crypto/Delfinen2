8de18a890cf6db1606f8833535a733acf8afc064
Tobiascph <tobias.zimmer@hotmail.com>
2020-05-11 10:02:16 +0200

    Hejsa

--- src/main/java/Mapper/MedlemsMapper.java
+++ src/main/java/Mapper/MedlemsMapper.java
@@ -1,4 +1,26 @@
 package Mapper;
 
+import Util.DBConnector;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MedlemsMapper {
+
+    //Tilføj medlem
+    public void createPizzaInDB(Pizza pizza) {
+        String sqlQuery = "";
+        Connection connection = DBConnector.getInstance().getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            sqlQuery = "insert into pizzaer(Pizzanavn,Ingredienser,Pris) values ('"
+                    + pizza.getNavn() + "','"
+                    + pizza.getIngredienser() + "',"
+                    + pizza.getPris() + ");";
+            statement.executeUpdate(sqlQuery);
+        } catch (SQLException e) {
+            System.out.println("Fejl: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/Model/Medlem.java
+++ src/main/java/Model/Medlem.java
@@ -1,4 +1,16 @@
 package Model;
 
+
+/*
+create table medlemmer (
+MedlemsID int not null auto_increment,
+Navn varchar(255) default null,
+Aargang int not null,
+Betalt boolean,
+Aktiv boolean,
+primary key(MedlemsID)
+)engine=InnoDB default charset=utf8;
+ */
+
 public class Medlem {
 }
